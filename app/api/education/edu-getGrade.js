eabb64726a096452b4b14f38e3c872b4b6cad91d
smackxu <smackxu@raventech.com>
2017-02-16 14:46:42 +0800

    add getGrage api

---
+++ app/api/education/edu-getGrade.js
@@ -0,0 +1,81 @@
+const SimulateLogin = require('./util/simulateLogin').SimulateLogin;
+const cheerio = require("cheerio");
+const iconv = require("iconv-lite");
+const charset = require('superagent-charset');
+const superagent = charset(require('superagent'));
+
+// 浏览器请求报文头部部分信息
+const browserMsg = {
+  "Accept-Encoding": "gzip, deflate",
+  "Origin": "http://jwzx.hrbust.edu.cn",
+  'Content-Type': 'application/x-www-form-urlencoded',
+};
+
+function handleGrade(cookie, year, term) {
+  const promise = new Promise((resolve, reject) => {
+    superagent
+      .get("http://jwzx.hrbust.edu.cn/academic/manager/score/studentOwnScore.do?groupId=&moduleId=")
+      .charset()
+      // .data({
+      //   year,
+      //   term,
+      // })
+      .set(browserMsg)
+      .set("Cookie", cookie)
+      .end((err, response, body) => {
+        if (err) {
+          resolve({
+            error: err,
+          });
+        } else {
+          const body = response.text;
+          const $ = cheerio.load(body);
+          const datalist = $('.datalist').find('tr');
+          // console.log(datalist);
+          // console.log($());
+          const result = {};
+          result.data = [];
+
+          datalist.each((index, item) => {
+            if (index === 0) {
+              return;
+            }
+            const innerItems = $(item).find('td');
+            const innerTexts = [];
+            innerItems.each((indexI, itemI) => {
+              let str = $(itemI).text().replace(/(\ +)|([ ])|([\r\n])/g, '');
+              innerTexts.push(str);
+            });
+            result.data.push(innerTexts);
+          });
+          resolve(result);
+        }
+      });
+  });
+  return promise;
+}
+
+function getGrade(params) {
+  var SimulateLoginParams = {
+    username: params.username,
+    password: params.password,
+    callback: function(result) {
+      if (result.error) {
+        params.callback({
+          error: result.error
+        });
+      } else {
+        handleGrade(result.cookie, params.year, params.term).then((result) => {
+          params.callback(result);
+        }).catch(function () {
+          console.log("Promise Rejected");
+        });
+      }
+    },
+    simulateIp: params.simulateIp,
+    yourCookie: params.yourCookie
+  }
+  new SimulateLogin(SimulateLoginParams);
+}
+
+exports.getGrade = getGrade;
