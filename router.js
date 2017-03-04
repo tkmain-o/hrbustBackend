b0460994932f9ea2f0f7592cfd1093338859555d
kira <13089703981@163.com>
2017-03-04 09:11:10 +0800

    增加四级查询接口

--- router.js
+++ router.js
@@ -1,23 +1,29 @@
-"use strict";
-var express = require('express');
+const http = require('http');
+var decoder = require('ling-cet-decoder');
+var request = require('request');
 
-var simulateLogin = require('./simulateLogin.js');
-
-var app = express();
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By", ' 3.2.1')
-  res.header("Content-Type", "application/json;charset=utf-8");
-  next();
+var name = "毛宇顺",
+  school = "海南大学",
+ cetType = 1; //1 for CET4, 2 for CET6
+const server = http.createServer((req, res) => {
+    var tik = '';
+    request.post({
+    url: 'http://find.cet.99sushe.com/search',
+    encoding: null,
+    body: decoder.getEncryptReqBody(cetType, school, name)
+    }, function (err, req, bodyBuf) {
+    if (err) {
+        throw new Error(err);
+    }
+    var ticket = decoder.decryptResBody(bodyBuf);
+    tik = ticket;
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World'+ticket+'');
+    console.log("我的准考证号是:", ticket);
+    });
 });
 
-app.get('/course', function(req, res1) {
-  console.log(req.query.username);
-  console.log(req.query.password);
-  var a = simulateLogin(req.query.username, req.query.password);
-  console.log(a);
+server.listen(3000, () => {
+    console.log(`node server is now running/`);
 });
-app.listen(4001);
-console.log('Listening on port 4001');
