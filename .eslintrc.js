806571a5f3333b3e318b7fb95b4f5384b92b970b
smackgg <smackgg@gmail.com>
2017-03-06 12:33:48 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "new-cap": ["error", { "capIsNew": false }],
+    "no-console": [2, { "allow": ["warn", "error"] }],
+    "no-unused-expressions": ["error", { "allowShortCircuit": true }],
+    "radix": ["error", "as-needed"]
+  },
+};
