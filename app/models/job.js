1d84e79b0c6082e6b26ca67f5c868078cecd7543
smackxu <smackxu@raventech.com>
2017-03-29 13:01:27 +0800

    add mongoose model news and job

--- app/models/job.js
+++ app/models/job.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 
-const News = new mongoose.Schema({
+const Job = new mongoose.Schema({
   id: { type: Number, unique: true, required: true, index: true },
   title: { type: String, required: true },
   date: { type: String, required: true },
   imageName: { type: String, required: true },
-  top: { type: Boolean, required: true },
-  sortId: { type: Number, unique: true, required: true, index: true },
 }, {
   timestamps: true,
 });
 
-module.exports = mongoose.model('news', News);
+module.exports = mongoose.model('job', Job);
