b4cd9fb0f7398f80ba86183fc740bbfffcf762c2
smackgg <smackgg@gmail.com>
2017-03-29 13:02:13 +0800

    add some handle mongo function in mongoUtils.js

--- app/spider/mongoUtils.js
+++ app/spider/mongoUtils.js
@@ -1,4 +1,5 @@
 const News = require('../models/news');
+const Job = require('../models/job');
 const log = require('bole')('mongo');
 
 // News.create({
@@ -20,6 +21,7 @@ const log = require('bole')('mongo');
 // });
 const collectionList = {
   News,
+  Job,
 };
 
 function isExisted(collection, query) {
@@ -39,8 +41,7 @@ function isExisted(collection, query) {
 }
 
 function insert(collection, value) {
-  // 检查数据库中是否存在
-  log.info(value.newsId);
+  // 插入数据库
   const promise = new Promise((resolve, reject) => {
     collectionList[collection].create(value).then(() => {
       log.info('添加成功');
@@ -56,5 +57,68 @@ function insert(collection, value) {
   return promise;
 }
 
+function update(collection, query, updates) {
+  // 更新数据库
+  const promise = new Promise((resolve, reject) => {
+    collectionList[collection].update(query, updates, { multi: true }).then(() => {
+      log.info('更新成功');
+      resolve();
+    }).catch((err) => {
+      if (err.code === 11000) {
+        log.warn('已经存在');
+        resolve();
+      }
+      reject(err);
+    });
+  });
+  return promise;
+}
+
+function findMax(collection, name) {
+  // 查找某一键值中的最大值
+  const promise = new Promise((resolve) => {
+    collectionList[collection]
+      .findOne()
+      .sort(`-${name}`)
+      .exec((err, member) => {
+        // your callback code
+        let id;
+        if (member) {
+          id = member.id;
+        }
+        resolve(id);
+      });
+  });
+  return promise;
+}
+
+function getDataPagination(collection, sortName, page, num) {
+  // 查找某一键值中的最大值
+
+  const startCount = (page - 1) * num;
+  const promise = new Promise((resolve) => {
+    collectionList[collection].count({}, (err, count) => {
+      if (startCount > count) {
+        // 超出数据库中数据数量
+        // log.info('超出数据库中数据数量');
+        resolve([]);
+      } else {
+        collectionList[collection]
+          .find()
+          .sort(`-${sortName}`)
+          .skip(startCount)
+          .limit(num)
+          .exec((errS, docs) => {
+            resolve(docs);
+          });
+      }
+    });
+  });
+  return promise;
+}
+
 exports.isExisted = isExisted;
 exports.insert = insert;
+exports.findMax = findMax;
+exports.update = update;
+exports.getDataPagination = getDataPagination;
