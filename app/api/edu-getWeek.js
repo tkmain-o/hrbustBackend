3e523e864ac5f244adbea334f4b3d4c78c81419e
smackxu <smackxu@raventech.com>
2017-03-29 15:17:49 +0800

    remove get random ip

--- app/api/edu-getWeek.js
+++ app/api/edu-getWeek.js
@@ -2,19 +2,10 @@ const cheerio = require('cheerio');
 const charset = require('superagent-charset');
 const superagent = charset(require('superagent'));
 
-const getRandomIp = () => {
-  const arr = [];
-  for (let i = 0; i < 4; i += 1) {
-    arr.push(Math.floor(Math.random() * 255));
-  }
-  return arr.join('.');
-};
-
 const browserMsg = {
   'Accept-Encoding': 'gzip, deflate',
   Origin: 'http://jwzx.hrbust.edu.cn',
   'Content-Type': 'application/x-www-form-urlencoded',
-  'X-Forwarded-For': `${getRandomIp()}`,
 };
 
 
