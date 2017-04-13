15c867774efec49c60ef4cde31810c0323751e4b
smackgg <smackgg@gmail.com>
2017-04-13 22:42:01 +0800

    udpate

--- app/api/util/captcha.js
+++ app/api/util/captcha.js
@@ -4,9 +4,10 @@ const fs = require('fs');
 
 let count = 0;
 const spiKeys = [
-  'ad6be37ba1c990d2faY7WmCyKfGkRcAA90nwglVA4V84JynHFE9lyPIosVFb0PijEwMP9BWgKciII',
-  '7a035b90a8142c343eq9CThVaZK2nbB9kYb1LrOeCxqtH0wl7upz8Hk8pii90sXv6e1kd6qHQ9',
-  'd5318ae3ade8ffa46fU4NyBvRaaIset9DRkTtw0N5ABttC0pL2nT53PNjjAkOciTpkgHd2bbEJbA',
+  // 'ad6be37ba1c990d2faY7WmCyKfGkRcAA90nwglVA4V84JynHFE9lyPIosVFb0PijEwMP9BWgKciII',
+  // '7a035b90a8142c343eq9CThVaZK2nbB9kYb1LrOeCxqtH0wl7upz8Hk8pii90sXv6e1kd6qHQ9',
+  // 'd5318ae3ade8ffa46fU4NyBvRaaIset9DRkTtw0N5ABttC0pL2nT53PNjjAkOciTpkgHd2bbEJbA',
+  '51ef8dc7b3b0b5be53qUWQPIpynwaRCZb2MxvzUXqlsPQYPtbp0CG0Df55dh9vp5BScpESjJcH',
 ];
 function getCaptcha(filePath) {
   if (count >= spiKeys.length - 1) {
@@ -23,6 +24,7 @@ function getCaptcha(filePath) {
       url: `http://lab.ocrking.com/ok.html?service=OcrKingForPhoneNumber&language=eng&charset=11&apiKey=${spiKeys[count]}&type=http://t.51chuli.com/contact/d827323fa035a7729w060771msa9211z.gif`,
       formData,
     }, (err, httpResponse, body) => {
+      // console.log(body);
       if (err) {
         console.error('upload failed:', err);
         resolve({
