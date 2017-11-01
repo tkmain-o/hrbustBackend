965c807df9cd2a57492ef54d3eae91304ceba748
smackgg <smackgg@gmail.com>
2017-11-02 03:16:27 +0800

    use new config

--- app/config.js
+++ app/config.js
@@ -1,3 +1,5 @@
+const superConfig = require('../smackgg-config/hrbust-backend-config.json');
+
 const config = module.exports;
 const PRODUCTION = process.env.NODE_ENV === 'production';
 // const DEV = process.env.NODE_ENV === 'dev';
@@ -7,9 +9,8 @@ config.express = {
   ip: '127.0.0.1',
 };
 
-config.mongodb = 'mongodb://localhost:27017/hrbust';
+config.mongodb = `mongodb://${superConfig.user}:${superConfig.password}@${superConfig.host}:${superConfig.port}/${superConfig.database}?authSource=${superConfig.authSource}`;
 
 if (PRODUCTION) {
   config.express.ip = '0.0.0.0';
-  config.mongodb = 'mongodb://107.170.52.153:27017/hrbust';
 }
--- app/server.js
+++ app/server.js
@@ -1,22 +1,24 @@
 const app = require('./index');
 const config = require('./config');
 const mongoose = require('mongoose');
-const bole = require('bole');
+const debug = require('debug')('server');
 
-bole.output({ level: 'debug', stream: process.stdout });
-const log = bole('server');
+// const bole = require('bole');
+
+// bole.output({ level: 'debug', stream: process.stdout });
+// const log = bole('server');
 mongoose.Promise = global.Promise;
 mongoose
   .connect(config.mongodb)
   .connection
-  .on('error', log.error)
+  .on('error', debug)
   .once('open', () => {
     app.listen(config.express.port, config.express.ip, (error) => {
       if (error) {
-        log.error('Unable to listen for connections', error);
+        debug('Unable to listen for connections', error);
         process.exit(10);
       }
-      log.info(`express is listening on http://${config.express.ip}:${config.express.port}`);
+      debug(`express is listening on http://${config.express.ip}:${config.express.port}`);
     });
-    log.info('server process starting');
+    debug('server process starting');
   });
