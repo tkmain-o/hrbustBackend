0bf159f098ea38430f81c67b3c007c14ea37c282
smackgg <smackgg@gmail.com>
2018-01-17 12:05:22 +0800

    get week api add term,year,week response

--- app/api/edu-getWeek.js
+++ app/api/edu-getWeek.js
@@ -26,8 +26,20 @@ function getWeek() {
           const $ = cheerio.load(body);
           const result = $('#date span').text();
           const thisWeek = result.replace(/\s/g, '');
+
+          const week = parseInt(thisWeek.match(/第(\w*)周/)[1]);
+          const terms = thisWeek.match(/(\w*)(秋|春)/);
+          const year = parseInt(terms[1]) - 1980;
+          const termsObj = {
+            春: 1,
+            秋: 2,
+          };
+          const term = termsObj[terms[2]];
           resolve({
             thisWeek,
+            term,
+            year,
+            week,
           });
         }
       });
