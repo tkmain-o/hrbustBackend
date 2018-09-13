5a6463f834da704d9a06a260f51b280b1091d6b9
smackgg <smackgg@gmail.com>
2018-09-13 12:58:03 +0800

    update user models and methods

--- models/user.js
+++ models/user.js
@@ -2,20 +2,57 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const studentInfos = new mongoose.Schema({
-  openId: {
+const UserSchema = new mongoose.Schema({
+  openid: {
     type: String,
+    required: true,
+    unique: true,
   },
   students: [{
     type: Schema.Types.ObjectId,
     ref: 'StudentInfos',
   }],
-  name: String,
-  avatar: String,
-  about: String,
-  social: String,
+  userInfo: {
+    nickName: String,
+    gender: String,
+    language: String,
+    city: String,
+    province: String,
+    country: String,
+    avatarUrl: String,
+  },
+  session_key: {
+    type: String,
+    required: true,
+  },
 }, {
   timestamps: true,
 })
 
-module.exports = mongoose.model('StudentInfos', studentInfos)
+class UserClass {
+  static async findByOpenIdAndUpdateUserInfo (data) {
+    // 查找是否有该用户，如果有更新用户信息，如果没有新建
+    const { openid } = data
+    let user = await this.findOne({
+      openid,
+    })
+    let result
+    if (user) {
+      user.set({
+        ...data,
+      })
+      result = await user.save()
+    } else {
+      const u = new this({
+        ...data,
+      })
+      result = await u.save()
+    }
+    return result
+  }
+}
+UserSchema.loadClass(UserClass)
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
