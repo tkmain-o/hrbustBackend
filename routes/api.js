5184973d828b6668eedab37e34e4890641dc3d5f
smackgg <smackgg@gmail.com>
2018-09-13 13:24:42 +0800

    remove comment'

--- routes/api.js
+++ routes/api.js
@@ -7,27 +7,4 @@ router.get('/wx-login', async (ctx) => wxLogin(ctx))
 
 router.put('/userinfo', async (ctx) => updateUserInfo(ctx))
 
-// router.get('/login', async (ctx) => {
-//   const { code } = ctx.request.query
-//   if (!code) ctx.throw(500)
-//   await wxLogin(ctx)
-// })
-
-
-// router.get('/login', async (ctx) => {
-//   const { code } = ctx.request.query
-//   if (!code) ctx.throw(500)
-// })
-
-//
-// router.get('/string', async (ctx) => {
-//   ctx.body = 'koa2 string'
-// })
-//
-// router.get('/json', async (ctx) => {
-//   ctx.body = {
-//     title: 'koa2 json',
-//   }
-// })
-
 module.exports = router
