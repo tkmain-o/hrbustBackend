1e01a7142994f4bd058b6497e5683e7d4fd3abd8
smackgg <smackgg@gmail.com>
2018-10-20 10:21:03 +0800

    add constans config

---
+++ constants/errorCode.js
@@ -0,0 +1,22 @@
+const errorCode = {
+  400001: {
+    message: '教务在线登录错误',
+  },
+  400002: {
+    message: '验证码识别错误',
+  },
+  400003: {
+    message: '获取验证码错误',
+  },
+  400004: {
+    message: '获取 cookie 错误',
+  },
+}
+
+exports.errorCode = errorCode
+
+exports.getErrorData = ({ error, code, message }) => ({
+  error: error || { message },
+  message: (error && error.message) || message || (errorCode[code] && errorCode[code].message),
+  code,
+})
