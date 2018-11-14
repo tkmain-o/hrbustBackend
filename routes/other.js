130f25640434c36fe7deaa9c250ce63d5e6ffbb1
smackgg <smackgg@gmail.com>
2018-11-14 16:26:02 +0800

    move news api to public api

--- routes/other.js
+++ routes/other.js
@@ -7,6 +7,11 @@ const {
   getBanner,
 } = require('../controller/other/banner')
 
+const {
+  getNews,
+  getNewsDetail,
+} = require('../controller/hrbust/news')
+
 router.prefix('/api')
 
 // 上传文件接口 七牛云
@@ -14,5 +19,9 @@ router.post('/media', ctx => media(ctx))
 
 // 获取首页 banner
 router.get('/banner', ctx => getBanner(ctx))
+
+// 教务在线
+router.get('/news', ctx => getNews(ctx))
+router.get('/news/:id', ctx => getNewsDetail(ctx))
 
 module.exports = router
